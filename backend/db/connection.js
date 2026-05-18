8e2e0d4b4a6449f44773d999d49b0c61f2307b10
Ayushpokar <ayushrpokar@gmail.com>
2026-05-18 16:58:10 +0530

    changes in database

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,16 +1,11 @@
 import { Pool } from "pg";
 
-const hostname = process.env.DB_HOSTNAME;
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-const database = process.env.DB_DATABASE;
 
 export const pool = new Pool({
-    user: username,
-    password : password,
-    host: hostname,
-    database : database,
-    port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 export default pool;    

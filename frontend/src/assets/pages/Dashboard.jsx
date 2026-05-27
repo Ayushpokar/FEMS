78a50c854fb179f4261b98e26f201abfe7db2f64
Ayushpokar <ayushrpokar@gmail.com>
2026-05-27 16:56:27 +0530

    Changes in dashboard

--- frontend/src/assets/pages/Dashboard.jsx
+++ frontend/src/assets/pages/Dashboard.jsx
@@ -77,7 +77,7 @@ export function Dashboard() {
     return (
         <>
             <div className="mb-6">
-                <h3 className="text-3xl font-bold text-gray-900">{role === "hod"? HOD : Faculty} Dashboard</h3>
+                <h3 className="text-3xl font-bold text-gray-900">{role === "hod"? "HOD" : "Faculty"} Dashboard</h3>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
 

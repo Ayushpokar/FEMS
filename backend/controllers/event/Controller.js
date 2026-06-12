8b658cbf29e3469a62b14734dcee2729aa9fb4ab
Ayushpokar <ayushrpokar@gmail.com>
2026-06-12 13:07:11 +0530

    email modified

--- backend/controllers/event/Controller.js
+++ backend/controllers/event/Controller.js
@@ -15,11 +15,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: "pokarayushr@gmail.com",
         pass: "vucu dafc dwll kltp"
-    }
+    },
+    family: 4
 });
 
 
